4aae80e56b4d92d059630b9d960279059f08d9f9
nico1018 <roscianinicolas99@gmail.com>
2023-09-26 18:59:38 -0300

    Agregando App

---
+++ AppCoder/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Create your models here.
+class Curso(models.Model):
+    curso = models.CharField(max_length=20)
+    camada = models.IntegerField()
+
+
+class Estudiate(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    email = models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40)
+    profesion = models.CharField(max_length=20)
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=20)
+    fecha_de_entrega = models.CharField(max_length=20)
+    entregado = models.BooleanField()
